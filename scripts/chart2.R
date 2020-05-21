3aa0595e9978f0b1390994809056a1fa8a0978ad
Zhenyu <stevenzy@uw.edu>
2020-05-21 15:53:47 -0700

    updated

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -3,13 +3,17 @@ library(dplyr)
 
 tuition <- read.csv("tuition_cost.csv", stringsAsFactors = FALSE)
 
+# pass "filtered" as param
 filtered <- filter(tuition, !is.na(room_and_board))
 
 # returns a graph that plots in-state tuition and room & board.
-room_vs_instate <- ggplot(data = filtered) +
-  geom_hex(mapping = aes(x = room_and_board, y = in_state_total)) +
-  labs(
-    title = "Tuition v Room ",
-    x = "Room and Board",
-    y = "In-state Tuition"
-  )
+hex_plot <- function(df) {
+  room_vs_instate <- ggplot(data = df) +
+    geom_hex(mapping = aes(x = room_and_board, y = in_state_total)) +
+    labs(
+      title = "Tuition v Room ",
+      x = "Room and Board",
+      y = "In-state Tuition"
+    )
+  return(room_vs_instate)
+}
--- scripts/chart3.R
+++ scripts/chart3.R
@@ -6,12 +6,14 @@ tuition <- read.csv("tuition_cost.csv", stringsAsFactors = FALSE)
 filtered <- filter(tuition, !is.na(room_and_board))
 
 # Returns a graph that shows in-state tuition vs degree length, separated by type of universities.
-instate_vs_degree_length <- ggplot(data = filtered) +
-  geom_bin2d(mapping = aes(x = degree_length, y = in_state_total)) +
-  facet_wrap(~type) +
-  labs(
-    title = "In-state tuition v Degree Length ",
-    x = "degree length",
-    y = "In-state Tuition"
-  )
-
+bin_plot <- function(df) {
+  instate_vs_degree_length <- ggplot(data = df) +
+    geom_bin2d(mapping = aes(x = degree_length, y = in_state_total)) +
+    facet_wrap(~type) +
+    labs(
+      title = "In-state tuition v Degree Length ",
+      x = "degree length",
+      y = "In-state Tuition"
+    )
+  return(instate_vs_degree_length)
+}
