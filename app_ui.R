a9257cc22cfff05a43196895d5fb8fa7fd59c365
EvelynSun <sun00@uw.edu>
2020-06-02 18:45:11 -0700

    some changes to Indro

--- app_ui.R
+++ app_ui.R
@@ -1,14 +1,19 @@
 # ui.R
 library(shiny)
-
+library(shinyWidgets)
 ui <- fluidPage(
   titlePanel("The US College Tuition Analysis",
              windowTitle = "College tuition explorer"),
-  tags$img(src='data/tuition.png', width=300),
+  tags$img("tuition image", src="data/tuition.jpg"), 
+  tags$h3("Overview"),
   tags$p("This interactive report is generated to explore the data of tuition
           and fees by college/university for 2018-2019, along with school type,
-          degree length, state, in-state vs out-of-state"),
-  tags$br(),
+          degree length, state, in-state vs out-of-state."),
+  tags$h3("Where the data is from:"),
+  tags$p("The data comes from many different sources but originally US Department 
+         community with powerful tools and resources to help users achieve their
+         data science goals."),
+  tags$h3("Questions:"),
   tags$p("Major questions that you can find answers in this report:"),
   tags$li("What is geographical distribution like with regard to
            different school types?",
@@ -17,5 +22,11 @@ ui <- fluidPage(
   tags$li("Question 2",
           tags$ul("To answer this question, we used ")),
   tags$li("Question 3",
-          tags$ul("To answer this question, we used "))
+          tags$ul("To answer this question, we used ")),
+  setBackgroundColor(
+    color = "LightBlue",
+    gradient = c("linear", "radial"),
+    direction = c("bottom", "top", "right", "left"),
+    shinydashboard = FALSE
+  )
 )
