a15f0342f80dc75039e6c69700ee2ec0c7f5a820
Yanbin Sun <binsmac@Bins-MacBook-Pro.local>
2020-06-05 12:00:37 +0900

    fixed indentation in app_ui

--- app_ui.R
+++ app_ui.R
@@ -19,38 +19,39 @@ ui <- navbarPage(
   title = "The US College Tuition Analysis",
   #Introduction page
   tabPanel("Introduction",
-  mainPanel(
-  tags$h3("Overview"),
-  tags$p("This interactive report is generated to explore the data of tuition
-          and fees by college/university for 2018-2019, along with school type,
-          degree length, state, in-state vs out-of-state."),
-  img( src="tuition.JPG",height = 350, width = 450), 
-  tags$h3("Where the data is from:"),
-  tags$p("The data come from many different sources but originally US Department 
-         community with powerful tools and resources to help users achieve their
-         data science goals. The dataset was found ",
-         tags$a(href="https://www.chronicle.com/interactives/tuition-and-fees",
+    mainPanel(
+      tags$h3("Overview"),
+      tags$p("This interactive report is generated to explore the data of tuition
+             and fees by college/university for 2018-2019, along with school type,
+              degree length, state, in-state vs out-of-state."),
+      img( src="tuition.JPG",height = 350, width = 450), 
+      tags$h3("Where the data is from:"),
+      tags$p("The data come from many different sources but originally US Department 
+             community with powerful tools and resources to help users achieve their
+            data science goals. The dataset was found ",
+             tags$a(href="https://www.chronicle.com/interactives/tuition-and-fees",
                 "The Cronicle of Higher Education"),),
-  tags$h3("Questions:"),
-  tags$p("Major questions that you can find answers in this report:"),
-  tags$li("What is geographical distribution like with regard to
-           different school types?",
-          tags$ul("To answer this question, we used locations of
-                  three different school types in different states.")),
-  tags$li("Is there any difference between in and out-of state tuition
-          for each type of school? If so, how different?",
-          tags$ul("To answer this question, we used a visualization by plotting
-                  total in-state tuition vs total out-of-state tuition to see
-                  the relationship between them.")),
-  tags$li("Question 3",
-          tags$ul("To answer this question, we used ...")),
-  setBackgroundColor(
-    color = "LightBlue",
-    gradient = c("linear", "radial"),
-    direction = c("bottom", "top", "right", "left"),
-    shinydashboard = FALSE
-  )
-)),
+      tags$h3("Questions:"),
+      tags$p("Major questions that you can find answers in this report:"),
+      tags$li("What is geographical distribution like with regard to
+               different school types?",
+              tags$ul("To answer this question, we used locations of
+                       three different school types in different states.")),
+      tags$li("Is there any difference between in and out-of state tuition
+              for each type of school? If so, how different?",
+              tags$ul("To answer this question, we used a visualization by plotting
+                      total in-state tuition vs total out-of-state tuition to see
+                      the relationship between them.")),
+      tags$li("Question 3",
+              tags$ul("To answer this question, we used ...")),
+      setBackgroundColor(
+        color = "LightBlue",
+        gradient = c("linear", "radial"),
+        direction = c("bottom", "top", "right", "left"),
+        shinydashboard = FALSE
+      )
+    )
+  ),
   tabPanel(
     "Pie Chart",
     titlePanel("What is each type of school like in different state?"),
@@ -84,6 +85,9 @@ ui <- navbarPage(
                       choices = college_type), width = 4),
         
         mainPanel(
-          plotlyOutput(outputId = "plot1"))
-   )))
+          plotlyOutput(outputId = "plot1")
+        )
+      )
+    ),
+)
       
