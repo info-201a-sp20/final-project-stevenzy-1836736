09026fe9586b9956b047903b0319672316296aca
EvelynSun <sun00@uw.edu>
2020-06-04 18:02:32 -0700

    adding image & get rid of error!

--- app_ui.R
+++ app_ui.R
@@ -20,11 +20,11 @@ ui <- navbarPage(
   #Introduction page
   tabPanel("Introduction",
   mainPanel(
-  tags$img("tuition image", src="data/tuition.jpg"), 
   tags$h3("Overview"),
   tags$p("This interactive report is generated to explore the data of tuition
           and fees by college/university for 2018-2019, along with school type,
           degree length, state, in-state vs out-of-state."),
+  img( src="tuition.JPG",height = 350, width = 450), 
   tags$h3("Where the data is from:"),
   tags$p("The data come from many different sources but originally US Department 
          community with powerful tools and resources to help users achieve their
@@ -66,12 +66,14 @@ ui <- navbarPage(
         tags$p("The purpose of this pie chart is to show the number of proportion of three
              different types of school: Public, Private, and For profit in different states.
              Users could choose their like of state and find out the result. ")
-    ),
+    ))),
+    
+    
     tabPanel(
       "Scatterplot",
       titlePanel("Visual Representation of the Difference between in vs out State Tuition by Public and Private College"),
       p(
-        "The following graph shows in-state and out-of-state tuition separately of public, private, and for-profit colleges.",
+        "The following graph shows in-state and out-of-state tuition separately of public, private, and for-profit colleges."
       ), 
       
       sidebarLayout(
@@ -83,10 +85,5 @@ ui <- navbarPage(
         
         mainPanel(
           plotlyOutput(outputId = "plot1"))
-      )
-    )
-  )
-)
-)
-
-
+   )))
+      
