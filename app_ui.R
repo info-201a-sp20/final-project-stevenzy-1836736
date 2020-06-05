5d5d14cfb511557525f476624d69c333ac2863c0
Yanbin Sun <binsmac@Bins-MacBook-Pro.local>
2020-06-06 02:07:14 +0900

    added a css stylesheet in www directory

--- app_ui.R
+++ app_ui.R
@@ -15,71 +15,74 @@ allstate_df <- tuition %>%
 all.state <- unique(allstate_df$state)
 
 
-ui <- navbarPage( 
-  title = "The US College Tuition Analysis",
-  #Introduction page
-  tabPanel("Introduction",
-    mainPanel(
-      tags$h2("Overview"),
-      tags$p("This interactive report is generated to explore the data of
-              tuition and fees by college/university for 2018-2019,
-              along with school type, degree length, state,
-              in-state vs out-of-state."),
-      img(src="tuition.JPG",height = 350, width = 450), 
-      tags$h3("Where the data is from:"),
-      tags$p("The data come from many different sources but originally 
-              US Department community with powerful tools and resources
-              to help users achieve their data science goals.
-              The dataset was found ",
-              tags$a(href="https://www.chronicle.com/interactives/tuition-and-fees",
-                    "The Cronicle of Higher Education"
-                    ),"."
-             ),
-      tags$h3("Questions:"),
-      tags$p("Major questions that you can find answers in this report:"),
-      tags$li("What is geographical distribution like with regard to
-               different school types?",
-              tags$ul("To answer this question, we used locations of
-                       three different school types in different states.")),
-      tags$li("Is there any difference between in and out-of state tuition
-              for each type of school? If so, how different?",
-              tags$ul("To answer this question, we used a visualization by
-                      plotting total in-state tuition vs total out-of-state
-                      tuition to see the relationship between them."
-                      )
-              ),
-      tags$li("Question 3",
-              tags$ul("To answer this question, we used ...")),
-      setBackgroundColor(
-        color = "LightBlue",
-        gradient = c("linear", "radial"),
-        direction = c("bottom", "top", "right", "left"),
-        shinydashboard = FALSE
-      )
-    )
-  ),
-  # Pie chart page
-  tabPanel(
-    "Pie Chart",
-    titlePanel("What is each type of school like in different state?"),
-    sidebarLayout(
-      sidebarPanel(
-        # select the state of user's interest
-        selectInput(inputId = "State",
-                    label = "Choose the State of your interest",
-                    choices = all.state), width = 4
-      ), 
-      mainPanel(
-        plotlyOutput("PiePlot"),
-        tags$p("The purpose of this pie chart is to show the number of
-                proportion of three different types of school:
-                Public, Private, and For profit in different states.
-                Users could choose their like of state and find out
-                the result."
-               )
-      )
-    )
-  ),
+ui <- fluidPage(includeCSS("www/style.css"),
+        navbarPage(
+          title = "The US College Tuition Analysis",
+          #Introduction page
+          tabPanel("Introduction",
+            mainPanel(
+              tags$h2("Overview"),
+              tags$p("This interactive report is generated to explore the data of
+                    tuition and fees by college/university for 2018-2019,
+                    along with school type, degree length, state,
+                    in-state vs out-of-state."),
+              img(src="tuition.JPG",height = 350, width = 450), 
+              tags$h3("Where the data is from:"),
+              tags$p("The data come from many different sources but originally 
+                     US Department community with powerful tools and resources
+                     to help users achieve their data science goals.
+                     The dataset was found ",
+                     tags$a(href="https://www.chronicle.com/interactives/tuition-and-fees",
+                            "The Cronicle of Higher Education"
+                           ),
+                     "."
+                    ),
+              tags$h3("Questions:"),
+              tags$p("Major questions that you can find answers in this report:"),
+              tags$li("What is geographical distribution like with regard to
+                     different school types?",
+                     tags$ul("To answer this question, we used locations of
+                             three different school types in different states.")),
+             tags$li("Is there any difference between in and out-of state tuition
+                      for each type of school? If so, how different?",
+                     tags$ul("To answer this question, we used a visualization by
+                             plotting total in-state tuition vs total out-of-state
+                             tuition to see the relationship between them."
+                            )
+                    ),
+             tags$li("Question 3",
+                      tags$ul("To answer this question, we calculated the mean
+                              of public and private shool tuition for each state.")
+                     ),
+      #setBackgroundColor(
+        #color = "LightBlue",
+        #gradient = c("linear", "radial"),
+        #direction = c("bottom", "top", "right", "left"),
+        #shinydashboard = FALSE
+      #)
+          )
+        ),
+        # Pie chart page
+        tabPanel(
+          "Pie Chart",
+          titlePanel("What is each type of school like in different state?"),
+          sidebarLayout(
+          sidebarPanel(
+            # select the state of user's interest
+            selectInput(inputId = "State",
+                        label = "Choose the State of your interest",
+                        choices = all.state), width = 4
+            ), 
+            mainPanel(
+              plotlyOutput("PiePlot"),
+              tags$p("The purpose of this pie chart is to show the number of
+                      proportion of three different types of school:
+                      Public, Private, and For profit in different states.
+                      Users could choose their like of state and find out
+                      the result.")
+            )
+        )
+    ),
     
   #Scatterplot page  
   tabPanel(
@@ -120,5 +123,6 @@ ui <- navbarPage(
            fees for every student."),
     tags$p("3. Acording to the interactive map, ...")
   )
+)
 )
       
