2274def994276ea31fd421f1290f595d8e8b63c4
Yanbin Sun <binsmac@Bins-MacBook-Pro.local>
2020-06-06 00:54:42 +0900

    fixed indentation in app_ui.R

--- app_ui.R
+++ app_ui.R
@@ -21,16 +21,20 @@ ui <- navbarPage(
   tabPanel("Introduction",
     mainPanel(
       tags$h2("Overview"),
-      tags$p("This interactive report is generated to explore the data of tuition
-             and fees by college/university for 2018-2019, along with school type,
-              degree length, state, in-state vs out-of-state."),
-      img( src="tuition.JPG",height = 350, width = 450), 
+      tags$p("This interactive report is generated to explore the data of
+              tuition and fees by college/university for 2018-2019,
+              along with school type, degree length, state,
+              in-state vs out-of-state."),
+      img(src="tuition.JPG",height = 350, width = 450), 
       tags$h3("Where the data is from:"),
-      tags$p("The data come from many different sources but originally US Department 
-             community with powerful tools and resources to help users achieve their
-            data science goals. The dataset was found ",
-             tags$a(href="https://www.chronicle.com/interactives/tuition-and-fees",
-                "The Cronicle of Higher Education"),),
+      tags$p("The data come from many different sources but originally 
+              US Department community with powerful tools and resources
+              to help users achieve their data science goals.
+              The dataset was found ",
+              tags$a(href="https://www.chronicle.com/interactives/tuition-and-fees",
+                    "The Cronicle of Higher Education"
+                    ),"."
+             ),
       tags$h3("Questions:"),
       tags$p("Major questions that you can find answers in this report:"),
       tags$li("What is geographical distribution like with regard to
@@ -39,9 +43,11 @@ ui <- navbarPage(
                        three different school types in different states.")),
       tags$li("Is there any difference between in and out-of state tuition
               for each type of school? If so, how different?",
-              tags$ul("To answer this question, we used a visualization by plotting
-                      total in-state tuition vs total out-of-state tuition to see
-                      the relationship between them.")),
+              tags$ul("To answer this question, we used a visualization by
+                      plotting total in-state tuition vs total out-of-state
+                      tuition to see the relationship between them."
+                      )
+              ),
       tags$li("Question 3",
               tags$ul("To answer this question, we used ...")),
       setBackgroundColor(
@@ -65,9 +71,12 @@ ui <- navbarPage(
       ), 
       mainPanel(
         plotlyOutput("PiePlot"),
-        tags$p("The purpose of this pie chart is to show the number of proportion of three
-             different types of school: Public, Private, and For profit in different states.
-             Users could choose their like of state and find out the result. ")
+        tags$p("The purpose of this pie chart is to show the number of
+                proportion of three different types of school:
+                Public, Private, and For profit in different states.
+                Users could choose their like of state and find out
+                the result."
+               )
       )
     )
   ),
@@ -111,11 +120,5 @@ ui <- navbarPage(
            fees for every student."),
     tags$p("3. Acording to the interactive map, ...")
   )
-  
-  
-  
-  
-  
-  
 )
       
